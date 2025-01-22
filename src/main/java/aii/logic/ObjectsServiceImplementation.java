db7d665ac0e745a9d180d51843c757c61a7430eb
Idan Dunsky <idan.dunsky@s.afeka.ac.il>
2025-01-22 23:37:51 +0200

    adjust entity to hold plant and related objects for optimal DB queries

--- src/main/java/aii/logic/ObjectsServiceImplementation.java
+++ src/main/java/aii/logic/ObjectsServiceImplementation.java
@@ -3,7 +3,9 @@ package aii.logic;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -28,7 +30,6 @@ public class ObjectsServiceImplementation implements EnhancedObjectsService {
 	private ObjectConverter converter;
 	private EmailValidator emailValidator;
 	private EnhancedUsersService users;
-	private final Float MILES_TO_KMS = 1.609344f;
 
 	public ObjectsServiceImplementation(ObjectsCrud objects, ObjectConverter converter, EnhancedUsersService users) {
 		this.objects = objects;
@@ -113,6 +114,24 @@ public class ObjectsServiceImplementation implements EnhancedObjectsService {
 		// validate if objectDetails exist or should be created
 		if (object.getObjectDetails() == null) {
 			object.setObjectDetails(new HashMap<>());
+		}else{
+			Map<String, Object> details = object.getObjectDetails();
+			Set<String> keys = details.keySet();
+			
+			if (keys.contains("relatedObjectId") &&
+						details.get("relatedObjectId") != null) {
+
+					String relatedObjectId = details.get("relatedObjectId").toString();
+					
+					if (!relatedObjectId.isBlank()) {
+
+						Optional<ObjectBoundary> op = this.objects.findById(relatedObjectId)
+								.map(this.converter::toBoundary);
+						if (op.isEmpty()) {
+							throw new InvalidInputException("Invalid relatedObjectId!");
+						}	
+					}
+				}
 		}
 		// INSERT object to db
 		return this.converter.toBoundary(this.objects.save(this.converter.toEntity(object)));
@@ -158,9 +177,8 @@ public class ObjectsServiceImplementation implements EnhancedObjectsService {
 			if (update.getAlias() != null && !update.getAlias().isBlank()) {
 				updatedObject.setAlias(update.getAlias());
 			}
-			
 
-			// if status updated - can't be blank 
+			// if status updated - can't be blank
 			if (update.getStatus() != null && !update.getStatus().isBlank()) {
 				updatedObject.setStatus(update.getStatus());
 			}
@@ -181,6 +199,67 @@ public class ObjectsServiceImplementation implements EnhancedObjectsService {
 			// if object details updated
 			if (update.getObjectDetails() != null) {
 				// add or update all objectDetails entries
+				Map<String, Object> details = update.getObjectDetails();
+				Set<String> keys = details.keySet();
+
+				if (keys.contains("currentSoilMoistureLevel") &&
+						details.get("currentSoilMoistureLevel") != null &&
+						details.get("currentSoilMoistureLevel") instanceof Integer) {
+
+					int value = (int) details.get("currentSoilMoistureLevel");
+					if (value <= 100 && value >= 0)
+						updatedObject.setCurrentSoilMoistureLevel(value);
+				}
+				details.remove("currentSoilMoistureLevel"); // for optimal storage in db
+
+				if (keys.contains("optimalSoilMoistureLevel") &&
+						details.get("optimalSoilMoistureLevel") != null &&
+						details.get("optimalSoilMoistureLevel") instanceof Integer) {
+
+					int value = (int) details.get("optimalSoilMoistureLevel");
+					if (value <= 100 && value >= 0)
+						updatedObject.setOptimalSoilMoistureLevel(value);
+				}
+				details.remove("optimalSoilMoistureLevel"); // for optimal storage in db
+
+				if (keys.contains("currentLightLevelIntensity") &&
+						details.get("currentLightLevelIntensity") != null &&
+						details.get("currentLightLevelIntensity") instanceof Integer) {
+
+					int value = (int) details.get("currentLightLevelIntensity");
+					if (value <= 100 && value >= 0)
+						updatedObject.setCurrentLightLevelIntensity(value);
+				}
+				details.remove("currentLightLevelIntensity"); // for optimal storage in db
+
+				if (keys.contains("optimalLightLevelIntensity") &&
+						details.get("optimalLightLevelIntensity") != null &&
+						details.get("optimalLightLevelIntensity") instanceof Integer) {
+
+					int value = (int) details.get("optimalLightLevelIntensity");
+					if (value <= 100 && value >= 0)
+						updatedObject.setOptimalLightLevelIntensity(value);
+				}
+				details.remove("optimalLightLevelIntensity"); // for optimal storage in db
+
+				if (keys.contains("relatedObjectId") &&
+						details.get("relatedObjectId") != null) {
+
+					String relatedObjectId = details.get("relatedObjectId").toString();
+					
+					if (!relatedObjectId.isBlank()) {
+
+						Optional<ObjectBoundary> op = this.objects.findById(relatedObjectId)
+								.map(this.converter::toBoundary);
+						if (op.isEmpty()) {
+							throw new InvalidInputException("Invalid relatedObjectId!");
+						}
+						updatedObject.setRelatedObjectId(relatedObjectId);
+					}
+
+				}
+				details.remove("relatedObjectId"); // for optimal storage in db
+
 				updatedObject.setObjectDetails(update.getObjectDetails());
 			}
 
@@ -201,7 +280,8 @@ public class ObjectsServiceImplementation implements EnhancedObjectsService {
 
 	@Override
 	@Transactional(readOnly = true)
-	public Optional<ObjectBoundary> getSpecificObject(String userSystemID, String userEmail, String objectSystemID, String objectId) {
+	public Optional<ObjectBoundary> getSpecificObject(String userSystemID, String userEmail, String objectSystemID,
+			String objectId) {
 		// check if any of the ID's are either empty or only whitespace
 		if (objectSystemID == null || objectId == null || objectSystemID.isBlank() || objectId.isBlank()) {
 			throw new InvalidInputException("objectSystemID and objectId can't be blank");
@@ -215,14 +295,14 @@ public class ObjectsServiceImplementation implements EnhancedObjectsService {
 		String objectKey = objectSystemID + "@@" + objectId;
 
 		switch (role) {
-		case ADMIN:
-			throw new UserUnauthorizedException("Get a specific object by ID is unauthorized for admin users!");
-		case END_USER:
-			return this.objects.findByObjectIdAndActiveTrue(objectKey).map(this.converter::toBoundary);
-		case OPERATOR:
-			return this.objects.findById(objectKey).map(this.converter::toBoundary);
-		default:
-			throw new IllegalArgumentException("Unexpected value: " + role);
+			case ADMIN:
+				throw new UserUnauthorizedException("Get a specific object by ID is unauthorized for admin users!");
+			case END_USER:
+				return this.objects.findByObjectIdAndActiveTrue(objectKey).map(this.converter::toBoundary);
+			case OPERATOR:
+				return this.objects.findById(objectKey).map(this.converter::toBoundary);
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + role);
 		}
 	}
 
@@ -257,12 +337,13 @@ public class ObjectsServiceImplementation implements EnhancedObjectsService {
 
 	@Override
 	@Transactional(readOnly = true)
-	public List<ObjectBoundary> getObjectsByLocation(double lat, double lng, double distance, String distanceUnits, String userSystemID, String userEmail, int page, int size) {
+	public List<ObjectBoundary> getObjectsByLocation(double lat, double lng, double distance, String distanceUnits,
+			String userSystemID, String userEmail, int page, int size) {
 		if (distance < 0.0) {
 			throw new InvalidInputException("Distance must be positive!");
 		}
 
-		if (!distanceUnits.equals("NEUTRAL") && !distanceUnits.equals("MILES") && !distanceUnits.equals("KILOMETERS")){
+		if (!distanceUnits.equals("NEUTRAL") && !distanceUnits.equals("MILES") && !distanceUnits.equals("KILOMETERS")) {
 			throw new InvalidInputException("Distance Units must be one of NEUTRAL, KILOMETERS and MILES!");
 		}
 
@@ -272,7 +353,8 @@ public class ObjectsServiceImplementation implements EnhancedObjectsService {
 				// find only active objects
 				// already sorted by query
 				return this.objects
-						.findAllWithinRadiusAndActiveIsTrue(lat, lng, distance, distanceUnits, PageRequest.of(page, size))
+						.findAllWithinRadiusAndActiveIsTrue(lat, lng, distance, distanceUnits,
+								PageRequest.of(page, size))
 						.stream()
 						.map(this.converter::toBoundary)
 						.toList();
@@ -294,24 +376,25 @@ public class ObjectsServiceImplementation implements EnhancedObjectsService {
 
 	@Override
 	@Transactional(readOnly = true)
-	public List<ObjectBoundary> getObjectsByType(String type, String userSystemID, String userEmail, int size, int page) {
+	public List<ObjectBoundary> getObjectsByType(String type, String userSystemID, String userEmail, int size,
+			int page) {
 
 		UserRole role = users.getUserRole(userSystemID, userEmail);
 
 		switch (role) {
-		case ADMIN:
-			throw new UserUnauthorizedException("Searching an object by type is unauthorized for admin users!");
-		case END_USER:
-			return this.objects
-					.findAllByTypeAndActiveTrue(type,
-							PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
-					.stream().map(this.converter::toBoundary).toList();
-		case OPERATOR:
-			return this.objects
-					.findAllByType(type, PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
-					.stream().map(this.converter::toBoundary).toList();
-		default:
-			throw new IllegalArgumentException("Unexpected value: " + role);
+			case ADMIN:
+				throw new UserUnauthorizedException("Searching an object by type is unauthorized for admin users!");
+			case END_USER:
+				return this.objects
+						.findAllByTypeAndActiveTrue(type,
+								PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
+						.stream().map(this.converter::toBoundary).toList();
+			case OPERATOR:
+				return this.objects
+						.findAllByType(type, PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
+						.stream().map(this.converter::toBoundary).toList();
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + role);
 		}
 	}
 
@@ -323,74 +406,76 @@ public class ObjectsServiceImplementation implements EnhancedObjectsService {
 		UserRole role = users.getUserRole(userSystemID, userEmail);
 
 		switch (role) {
-		case ADMIN:
-			throw new UserUnauthorizedException(
-					"Searching an object by type and status is unauthorized for admin users!");
-		case END_USER:
-			return this.objects
-					.findAllByTypeAndStatusAndActiveTrue(type, status,
-							PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
-					.stream().map(this.converter::toBoundary).toList();
-		case OPERATOR:
-			return this.objects
-					.findAllByTypeAndStatus(type, status,
-							PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
-					.stream().map(this.converter::toBoundary).toList();
-		default:
-			throw new IllegalArgumentException("Unexpected value: " + role);
+			case ADMIN:
+				throw new UserUnauthorizedException(
+						"Searching an object by type and status is unauthorized for admin users!");
+			case END_USER:
+				return this.objects
+						.findAllByTypeAndStatusAndActiveTrue(type, status,
+								PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
+						.stream().map(this.converter::toBoundary).toList();
+			case OPERATOR:
+				return this.objects
+						.findAllByTypeAndStatus(type, status,
+								PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
+						.stream().map(this.converter::toBoundary).toList();
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + role);
 		}
 	}
 
 	@Override
-    @Transactional(readOnly = true)
-    public List<ObjectBoundary> getObjectsByAlias(String alias, String userSystemID, String userEmail, int size, int page) {
-            UserRole role = users.getUserRole(userSystemID, userEmail);
+	@Transactional(readOnly = true)
+	public List<ObjectBoundary> getObjectsByAlias(String alias, String userSystemID, String userEmail, int size,
+			int page) {
+		UserRole role = users.getUserRole(userSystemID, userEmail);
 
-        switch (role) {
-            case ADMIN:
-                throw new UserUnauthorizedException("Searching an object by alias is unauthorized for admin users!");
+		switch (role) {
+			case ADMIN:
+				throw new UserUnauthorizedException("Searching an object by alias is unauthorized for admin users!");
 
-            case END_USER:
-                return this.objects
-                        .findAllByAliasAndActiveTrue(alias,
-                                PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
-                        .stream().map(this.converter::toBoundary).toList();
+			case END_USER:
+				return this.objects
+						.findAllByAliasAndActiveTrue(alias,
+								PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
+						.stream().map(this.converter::toBoundary).toList();
 
-            case OPERATOR:
-                return this.objects
-                        .findAllByAlias(alias,
-                                PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
-                        .stream().map(this.converter::toBoundary).toList();
-            default:
-                throw new IllegalArgumentException("Unexpected value: " + role);
-        }
-    }
+			case OPERATOR:
+				return this.objects
+						.findAllByAlias(alias,
+								PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
+						.stream().map(this.converter::toBoundary).toList();
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + role);
+		}
+	}
 
-
-    @Override
-    @Transactional(readOnly = true)
-    public List<ObjectBoundary> getObjectsByAliasPattern(String pattern, String userSystemID, String userEmail, int size, int page) {
+	@Override
+	@Transactional(readOnly = true)
+	public List<ObjectBoundary> getObjectsByAliasPattern(String pattern, String userSystemID, String userEmail,
+			int size, int page) {
 
 		UserRole role = users.getUserRole(userSystemID, userEmail);
 
-        switch (role) {
-            case ADMIN:
-                throw new UserUnauthorizedException("Searching an object by alias pattern is unauthorized for admin users!");
+		switch (role) {
+			case ADMIN:
+				throw new UserUnauthorizedException(
+						"Searching an object by alias pattern is unauthorized for admin users!");
 
-            case END_USER:
-                return this.objects
-                        .findAllByAliasLikeAndActiveTrue("%" + pattern + "%",
-                                PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
-                        .stream().map(this.converter::toBoundary).toList();
+			case END_USER:
+				return this.objects
+						.findAllByAliasLikeAndActiveTrue("%" + pattern + "%",
+								PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
+						.stream().map(this.converter::toBoundary).toList();
 
-            case OPERATOR:
-                return this.objects
-                        .findAllByAliasLike("%" + pattern + "%",
-                                PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
-                        .stream().map(this.converter::toBoundary).toList();
-            default:
-                throw new IllegalArgumentException("Unexpected value: " + role);
-        }
-    }
+			case OPERATOR:
+				return this.objects
+						.findAllByAliasLike("%" + pattern + "%",
+								PageRequest.of(page, size, Direction.DESC, "creationTime", "objectId"))
+						.stream().map(this.converter::toBoundary).toList();
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + role);
+		}
+	}
 
 }
--- src/main/java/aii/logic/converters/ObjectConverter.java
+++ src/main/java/aii/logic/converters/ObjectConverter.java
@@ -1,11 +1,15 @@
 package aii.logic.converters;
 
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.stereotype.Component;
+
+import aii.data.ObjectEntity;
+import aii.logic.CreatedBy;
+import aii.logic.Location;
 import aii.logic.ObjectBoundary;
 import aii.logic.ObjectId;
-import aii.logic.CreatedBy;
-import aii.data.ObjectEntity;
-import aii.logic.Location;
 
 @Component
 public class ObjectConverter {
@@ -21,7 +25,61 @@ public class ObjectConverter {
         entity.setCreationTime(boundary.getCreationTimestamp());
         entity.setCreatedBy(boundary.getCreatedBy().getUserId().getSystemID() + "@@"
                 + boundary.getCreatedBy().getUserId().getEmail());
-        entity.setObjectDetails(boundary.getObjectDetails());
+
+        Map<String, Object> details = boundary.getObjectDetails();
+        Set<String> keys = details.keySet();
+
+
+        if (keys.contains("currentSoilMoistureLevel") &&
+                details.get("currentSoilMoistureLevel") != null &&
+                details.get("currentSoilMoistureLevel") instanceof Integer) {
+
+            int value = (int) details.get("currentSoilMoistureLevel");
+            if( value <= 100 && value >=0 )
+                entity.setCurrentSoilMoistureLevel(value);
+        }
+        details.remove("currentSoilMoistureLevel"); // for optimal storage in db
+
+        if (keys.contains("optimalSoilMoistureLevel") &&
+                details.get("optimalSoilMoistureLevel") != null &&
+                details.get("optimalSoilMoistureLevel") instanceof Integer) {
+
+            int value = (int) details.get("optimalSoilMoistureLevel");
+            if( value <= 100 && value >=0 )
+                entity.setOptimalSoilMoistureLevel(value);
+        }
+        details.remove("optimalSoilMoistureLevel"); // for optimal storage in db
+
+        if (keys.contains("currentLightLevelIntensity") &&
+                details.get("currentLightLevelIntensity") != null &&
+                details.get("currentLightLevelIntensity") instanceof Integer) {
+
+            int value = (int) details.get("currentLightLevelIntensity");
+            if( value <= 100 && value >=0 )
+                entity.setCurrentLightLevelIntensity(value);
+        }
+        details.remove("currentLightLevelIntensity"); // for optimal storage in db
+
+        if (keys.contains("optimalLightLevelIntensity") &&
+                details.get("optimalLightLevelIntensity") != null &&
+                details.get("optimalLightLevelIntensity") instanceof Integer) {
+
+            int value = (int) details.get("optimalLightLevelIntensity");
+            if( value <= 100 && value >=0 )
+                entity.setOptimalLightLevelIntensity(value);
+        }
+        details.remove("optimalLightLevelIntensity"); // for optimal storage in db
+
+        if (keys.contains("relatedObjectId") &&
+                details.get("relatedObjectId") != null) {
+
+            String relatedObjectId = details.get("relatedObjectId").toString();
+            if(!relatedObjectId.isBlank())
+                entity.setRelatedObjectId(relatedObjectId);
+        }
+        details.remove("relatedObjectId"); // for optimal storage in db
+
+        entity.setObjectDetails(details);
         return entity;
     }
 
@@ -35,13 +93,36 @@ public class ObjectConverter {
         boundary.setLocation(new Location(entity.getLat(), entity.getLng()));
         boundary.setActive(entity.getActive());
         boundary.setCreationTimestamp(entity.getCreationTime());
-        
+
         String[] createdByString = entity.getCreatedBy().split("@@");
         String systemID = createdByString[0];
         String userEmail = createdByString[1];
 
         boundary.setCreatedBy(new CreatedBy(systemID, userEmail));
-        boundary.setObjectDetails(entity.getObjectDetails());
+
+        Map<String, Object> details = entity.getObjectDetails();
+
+        if (entity.getCurrentSoilMoistureLevel() != null) {
+            details.put("currentSoilMoistureLevel", entity.getCurrentSoilMoistureLevel());
+        }
+
+        if (entity.getOptimalSoilMoistureLevel() != null) {
+            details.put("optimalSoilMoistureLevel", entity.getOptimalSoilMoistureLevel());
+        }
+
+        if (entity.getCurrentLightLevelIntensity() != null) {
+            details.put("currentLightLevelIntensity", entity.getCurrentLightLevelIntensity());
+        }
+
+        if (entity.getOptimalLightLevelIntensity() != null) {
+            details.put("optimalLightLevelIntensity", entity.getOptimalLightLevelIntensity());
+        }
+
+        if (entity.getRelatedObjectId() != null) {
+            details.put("relatedObjectId", entity.getRelatedObjectId());
+        }
+
+        boundary.setObjectDetails(details);
         return boundary;
     }
 }
