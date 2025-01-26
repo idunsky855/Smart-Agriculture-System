86ad1ea16742a4c3201ce821d2c76170ac920b04
Liron <liron.bar@percepto.co>
2025-01-26 21:09:53 +0200

    mockup script for create and update plants

--- src/main/java/aii/presentation/ObjectController.java
+++ src/main/java/aii/presentation/ObjectController.java
@@ -1,7 +1,5 @@
 package aii.presentation;
 
-import java.util.Arrays;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
